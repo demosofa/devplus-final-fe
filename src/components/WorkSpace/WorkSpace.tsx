574d9cace2cc0948c99af55d975a6e04ba3d6253
Nguyen Thanh Nhan <nhandaica125@gmail.com>
2023-08-22 22:06:30 +0700

    feat:  add field action

--- src/components/WorkSpace/WorkSpace.tsx
+++ src/components/WorkSpace/WorkSpace.tsx
@@ -1,10 +1,17 @@
+import { CheckCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
 import { Table } from 'antd';
 import { useGetListWorkSpace } from 'hooks/useGetListWorkSpace';
 import { WORKSPACE } from 'types';
+import './WorkSpace.css';
 
 const WorkSpace = () => {
 	const { data: listWorkSpace } = useGetListWorkSpace();
 	const columns = [
+		{
+			title: 'id',
+			dataIndex: 'id',
+			key: 'id',
+		},
 		{
 			title: 'name',
 			dataIndex: 'title_workspace',
@@ -26,9 +33,29 @@ const WorkSpace = () => {
 			dataIndex: 'status',
 			key: 'status',
 		},
+		{
+			title: 'Action',
+			key: 'action',
+			render: (record: WORKSPACE) => (
+				<div className="action-icons">
+					{record.status === 'pending' && (
+						<>
+							<CheckCircleOutlined className="icons-check" />
+							<CloseCircleOutlined className="icons-close" />
+						</>
+					)}
+				</div>
+			),
+		},
 	];
 
-	return <Table<WORKSPACE> columns={columns} dataSource={listWorkSpace} />;
+	return (
+		<Table<WORKSPACE>
+			columns={columns}
+			dataSource={listWorkSpace}
+			rowKey={(record) => record.id}
+		/>
+	);
 };
 
 export default WorkSpace;
