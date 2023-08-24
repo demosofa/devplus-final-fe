0d01655f876df0bc76ea76d7226801d67fcc8dce
Nguyen Thanh Nhan <nhandaica125@gmail.com>
2023-08-24 16:45:11 +0700

    feat: create campaign

---
+++ src/hooks/useDetailWorkspace.ts
@@ -0,0 +1,18 @@
+import { QUERY_KEY } from '@constants';
+import { detailWorkSpace } from '@services';
+import { UseQueryResult, useQuery } from '@tanstack/react-query';
+import { WorkspaceType } from '@types';
+
+export const useDetailWorkspace = (
+	id: number
+): UseQueryResult<{
+	data: WorkspaceType[];
+}> => {
+	return useQuery({
+		queryKey: [QUERY_KEY.DETAIL_WORKSPACE],
+		queryFn: async () => {
+			const { data } = await detailWorkSpace(id);
+			return data;
+		},
+	});
+};
