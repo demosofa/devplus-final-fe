430da69b93e7621fbe365006f7f4963eba271d9f
datnvn <idolsupperman3215@gmail.com>
2023-08-25 16:20:17 +0700

    fix: dp31 change component notify

--- src/hooks/useUpdateCampaign.ts
+++ src/hooks/useUpdateCampaign.ts
@@ -1,12 +1,29 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
 import { updateCampaign } from 'services/campaign.service';
 import { CampaignType } from '@types';
+import { QUERY_KEY } from '@constants';
 
 export const useUpdateCampaign = () => {
+	const queryClient = useQueryClient();
+
 	return useMutation({
 		mutationFn: async (values: CampaignType) => {
 			const { data } = await updateCampaign(values);
 			return data;
+		},
+		onSuccess(data) {
+			queryClient.setQueryData<CampaignType[]>(
+				[QUERY_KEY.LIST_CAMPAIGN],
+				(listCampaign) => {
+					if (listCampaign) {
+						const idx = listCampaign.findIndex((item) => item.id == data.id);
+						const cloned = listCampaign.concat();
+						cloned[idx] = data;
+						return cloned;
+					}
+				}
+			);
 		},
 	});
 };
