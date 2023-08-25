1a1421bb60a6a1072883cd5dbec9920b1eaaae1a
datnvn <idolsupperman3215@gmail.com>
2023-08-25 16:20:17 +0700

    fix: dp31 change component notify

--- src/hooks/useUpdateCampaign.ts
+++ src/hooks/useUpdateCampaign.ts
@@ -17,7 +17,7 @@ export const useUpdateCampaign = () => {
 		onSuccess: (data) => {
 			notification.success({
 				message: NOTIFICATION.SUCCESS,
-				description: 'Create campaign successfully.',
+				description: 'Update campaign successfully.',
 			});
 			queryClient.setQueryData<CampaignType[]>(
 				[QUERY_KEY.LIST_CAMPAIGN],
@@ -34,7 +34,7 @@ export const useUpdateCampaign = () => {
 		onError: () => {
 			notification.error({
 				message: NOTIFICATION.ERROR,
-				description: 'Create campaign failed',
+				description: 'Update campaign failed',
 			});
 		},
 	});
