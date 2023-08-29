901cbfa8de7a629ce0c88c649768ccc789af49b3
demosofa <62540039+demosofa@users.noreply.github.com>
2023-08-29 09:53:26 +0700

    Hotfix (#33)
    * feat: add env file
    
    * fix: sort import exclude components and fix missing import
    
    * fix: remove files

--- src/services/campaign.service.ts
+++
@@ -1,9 +0,0 @@
-import { API_URL } from '@constants';
-import { CampaignType } from '@types';
-import axios from 'axios';
-
-export const updateCampaign = (data: CampaignType) =>
-	axios.patch<CampaignType>(API_URL.CAMPAIGN + data.id, data);
-
-export const getFindOneCampaign = (id: number) =>
-	axios.get(API_URL.CAMPAIGN + id);
