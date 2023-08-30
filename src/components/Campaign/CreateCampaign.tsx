274e427640358f9ee815fbe574fe5400a1ee22f0
datnvn <idolsupperman3215@gmail.com>
2023-08-30 14:30:51 +0700

    refactor: all page sprint 1

--- src/components/Campaign/CreateCampaign.tsx
+++ src/components/Campaign/CreateCampaign.tsx
@@ -1,16 +1,16 @@
 import { Button, Card, DatePicker, Form, Input, Space } from 'antd';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import dayjs from 'dayjs';
 import 'react-quill/dist/quill.snow.css';
 import ReactQuill from 'react-quill';
 import { RangePickerProps } from 'antd/es/date-picker';
+import { PlusCircleOutlined } from '@ant-design/icons';
 
 import './CreateCampaign.css';
 import { useCreateCampaign } from 'hooks/useCreateCampaign';
 import { CampaignType } from '@types';
-import { PlusCircleOutlined } from '@ant-design/icons';
 
 const CreateCampaign = () => {
 	dayjs.extend(customParseFormat);
@@ -53,6 +53,16 @@ const CreateCampaign = () => {
 	const handleDescriptionChange = (value: string) => {
 		setDescription(value);
 	};
+
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (registrationSuccess) {
+			setTimeout(() => {
+				navigate('/campaign');
+			}, 1000);
+		}
+	}, [navigate, registrationSuccess]);
 
 	return (
 		<div>
