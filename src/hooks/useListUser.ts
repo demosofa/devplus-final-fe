f8f0198d920652af523bf294fb67eb4806c56f04
datnvn <idolsupperman3215@gmail.com>
2023-09-04 12:13:49 +0700

    fix: list user dp54

--- src/hooks/useListUser.ts
+++ src/hooks/useListUser.ts
@@ -1,15 +1,16 @@
 import { UseQueryResult, useQuery } from '@tanstack/react-query';
 
 import { QUERY_KEY } from '@constants';
-import { PageMeta, UserType } from '@types';
+import { AuthPayload, PageMeta, UserType } from '@types';
 import { getListUser } from 'services/user.services';
 
 export const useGetListUser = (
+	auth: AuthPayload | void,
 	page: number,
 	pageSize: number
 ): UseQueryResult<PageMeta<UserType>> => {
 	return useQuery({
-		queryKey: [QUERY_KEY.LIST_USER, page, pageSize],
+		queryKey: [QUERY_KEY.LIST_USER, auth, page, pageSize],
 		queryFn: async () => {
 			const { data } = await getListUser(page, pageSize);
 			return data;
