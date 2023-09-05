f934e3effbe90da8937232533bfc4d760c370eeb
Nguyen Thanh Nhan <nhandaica125@gmail.com>
2023-09-05 11:32:36 +0700

    feat: create cv

--- src/hooks/useCreateCv.ts
+++ src/hooks/useCreateCv.ts
@@ -1,13 +1,10 @@
 import { useMutation } from '@tanstack/react-query';
 import { notification } from 'antd';
-import { useNavigate } from 'react-router-dom';
 
 import { NOTIFICATION } from '@enums';
 import { createCv } from '@services';
 
 export const useCreateCv = () => {
-	const navigate = useNavigate();
-
 	return useMutation({
 		mutationFn: async (values: FormData) => {
 			const { data } = await createCv(values);
@@ -18,7 +15,6 @@ export const useCreateCv = () => {
 				message: NOTIFICATION.SUCCESS,
 				description: 'Create cv successfully.',
 			});
-			navigate('/cv');
 		},
 		onError: () => {
 			notification.error({
