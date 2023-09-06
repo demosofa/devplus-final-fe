6b2e4f5d5d9f01cd52b30cf7e28f2587c9b795f2
Nguyen Thanh Nhan <nhandaica125@gmail.com>
2023-09-06 10:35:22 +0700

    fix: detail cv with iframe

--- src/pages/CvDetail/CvDetail.tsx
+++ src/pages/CvDetail/CvDetail.tsx
@@ -1,7 +1,7 @@
-import { Link, useParams } from 'react-router-dom';
-import { Card, Col, Form, Input, Row, Select, Spin } from 'antd';
+import { useParams } from 'react-router-dom';
+import { Card, Col, Form, Input, Row, Typography } from 'antd';
 import dayjs, { Dayjs } from 'dayjs';
-import { useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import 'react-quill/dist/quill.snow.css';
 
 import { CvType } from '@types';
@@ -10,148 +10,84 @@ import './CvDetail.css';
 import { useGetDetailCv } from 'hooks/useGetDetailCv';
 import { BASE_URL } from '@constants';
 
+const { Title } = Typography;
+
 export const CvDetail = () => {
 	const { id } = useParams();
 
-	const { data, isLoading: detailCampaignLoading } = useGetDetailCv(+id!);
-
-	const { Option } = Select;
-
-	const [displayType, setDisplayType] = useState('link');
+	const { data, isLoading: detailCvLoading } = useGetDetailCv(+id!);
 
 	const detailCv = useMemo(() => {
-		if (detailCampaignLoading || !data) {
+		if (detailCvLoading || !data) {
 			return undefined;
 		}
 
 		const cloned = clone(data) as Omit<CvType, 'expired_time'> & {
 			expired_time: Dayjs;
 		};
-
 		cloned.expired_time = dayjs(data.expired_time);
 
-		return cloned;
-	}, [data, detailCampaignLoading]);
+		if (cloned.file.includes('https://drive.google.com/')) {
+			cloned.file = cloned.file.replace('view', 'preview');
+		} else {
+			cloned.file = BASE_URL + cloned.file;
+		}
 
-	if (detailCampaignLoading) {
-		return (
-			<div className="isLoading">
-				<Spin />
-			</div>
-		);
-	}
+		return cloned;
+	}, [data, detailCvLoading]);
 
 	return (
-		<Card className="card-wrap-container">
-			<Row>
-				<Col span={24}>
-					<div className="main-container">
-						<Form
-							initialValues={detailCv}
-							name="complex-form"
-							labelCol={{
-								span: 3,
-							}}
-							wrapperCol={{
-								span: 16,
-							}}
-							className="full-form"
-						>
-							<Form.Item label="Name">
-								<Form.Item name="name">
-									<Input readOnly />
-								</Form.Item>
-							</Form.Item>
-							<Form.Item label="Email ">
-								<Form.Item name="email">
-									<Input readOnly />
-								</Form.Item>
-							</Form.Item>
-							<Form.Item label="Phone Number">
-								<Form.Item name="phone_number">
-									<Input readOnly />
-								</Form.Item>
-							</Form.Item>
-							<Form.Item label="Apply Position">
-								<Form.Item name="apply_position">
-									<Input readOnly />
-								</Form.Item>
-							</Form.Item>
-							<Form.Item label="Create At">
-								<Form.Item name="create_at">
-									<Input readOnly />
-								</Form.Item>
-							</Form.Item>
-							<Form.Item label="File" className="scrollable-card__form-item">
-								<Form.Item name="file">
-									<Select value={displayType} onChange={setDisplayType}>
-										<Option value="link">Link</Option>
-										<Option value="image">Image</Option>
-									</Select>
-								</Form.Item>
-								<Form.Item noStyle>
-									{displayType === 'link' ? (
-										detailCv?.file.startsWith('https://drive.google.com/') ? (
-											<Link
-												to={detailCv?.file}
-												target="_blank"
-												rel="noopener noreferrer"
-											>
-												{detailCv?.file}
-											</Link>
-										) : (
-											<Link to={`${BASE_URL}${detailCv?.file}`} target="_blank">
-												{detailCv?.file}
-											</Link>
-										)
-									) : detailCv?.file.startsWith('https://drive.google.com/') ? (
-										<object
-											data={`${BASE_URL}${detailCv?.file}`}
-											type="application/pdf"
-											width="100%"
-											height="500px"
-										>
-											<p>
-												Unable to display PDF file.{' '}
-												<Link
-													to={`${BASE_URL}${detailCv?.file}`}
-													target="_blank"
-												>
-													Download
-												</Link>
-												instead.
-											</p>
-										</object>
-									) : (
-										<object
-											data={`${BASE_URL}${detailCv?.file}`}
-											type="application/pdf"
-											width="100%"
-											height="500px"
-										>
-											<p>
-												Unable to display PDF file.{' '}
-												<Link
-													to={`${BASE_URL}${detailCv?.file}`}
-													target="_blank"
-												>
-													Download
-												</Link>
-												instead.
-											</p>
-										</object>
-									)}
-								</Form.Item>
-							</Form.Item>
-							<Form.Item label="Status ">
-								<Form.Item name="status">
-									<Input readOnly />
-								</Form.Item>
-							</Form.Item>
-						</Form>
-					</div>
-				</Col>
-			</Row>
-		</Card>
+		<Row className="Cv-container">
+			<Col span={24} md={12}>
+				<Card loading={detailCvLoading} style={{ height: '100%' }}>
+					<Typography>
+						<Title className="title-cv-detail">Cv Information</Title>
+					</Typography>
+					<Form
+						initialValues={detailCv}
+						name="complex-form"
+						labelCol={{
+							span: 6,
+						}}
+						wrapperCol={{
+							span: 18,
+						}}
+						className="full-form"
+					>
+						<Form.Item label="Name" name="name">
+							<Input readOnly />
+						</Form.Item>
+
+						<Form.Item label="Email " name="email">
+							<Input readOnly />
+						</Form.Item>
+
+						<Form.Item label="Phone Number" name="phone_number">
+							<Input readOnly />
+						</Form.Item>
+
+						<Form.Item label="Apply Position" name="apply_position">
+							<Input readOnly />
+						</Form.Item>
+
+						<Form.Item label="Create At" name="create_at">
+							<Input readOnly />
+						</Form.Item>
+
+						<Form.Item label="Status " name="status">
+							<Input readOnly />
+						</Form.Item>
+					</Form>
+				</Card>
+			</Col>
+			<Col span={24} md={12}>
+				<Card loading={detailCvLoading}>
+					<Typography>
+						<Title className="title-cv-detail">Cv File</Title>
+					</Typography>
+					<iframe src={detailCv?.file} className="iframe-cv-detail"></iframe>
+				</Card>
+			</Col>
+		</Row>
 	);
 };
