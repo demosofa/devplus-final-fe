0d14a9fd4407a22ad7d05ea31fa98fa8b459b5b6
demosofa <62540039+demosofa@users.noreply.github.com>
2023-09-09 21:32:38 +0700

    Feat/route (#72)
    * fix: in case roles is undefined in route based role
    
    * fix: make path / navigate to login instead

--- src/layouts/PublicLayout/PublicLayout.tsx
+++ src/layouts/PublicLayout/PublicLayout.tsx
@@ -1,21 +1,12 @@
 import { Layout } from 'antd';
-import { Outlet, useLocation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import './PublicLayout.css';
-import { TopBar } from 'components/TopBar/TopBar';
-import { useAuth } from '@hooks';
 
 const { Content } = Layout;
 
 export function PublicLayout() {
-	const { getAuth, setAuth } = useAuth();
-	const auth = getAuth();
-	const location = useLocation();
-
 	return (
 		<Layout className="layout-container">
-			{['login'].some((text) => location.pathname.includes(text)) ? null : (
-				<TopBar auth={auth} setAuth={setAuth} />
-			)}
 			<Content style={{ overflow: 'auto' }}>
 				<Outlet />
 			</Content>
