677ff8547eeec91337c4cde7fb3a8544530e2396
datnvn <idolsupperman3215@gmail.com>
2023-09-13 12:14:57 +0700

    fix: change width and height image

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -32,6 +32,8 @@ export function Login() {
 		<Row className="login-container">
 			<Col span={0} md={14} lg={16}>
 				<img
+					width={'100%'}
+					height={'100%'}
 					src="https://devskiller.com/wp-content/uploads/2022/05/Candidate-selection.png"
 					alt=""
 				/>
